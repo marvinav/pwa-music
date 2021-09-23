b51404bd583a88d48f2730e22c75c79f6142f637
marvinav <marvinav@outlook.com>
2021-09-23 14:40:05 +0300

    navbar story

---
+++ src/tests/layouts/NavBar.stories.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { NavBar } from '../../layouts/NavBar';
+import { DictionaryProvider } from '../__mocks__/dictionaryMock';
+
+export default {
+    component: NavBar,
+    title: 'Layouts/Navbar',
+    decorators: [(story) => <DictionaryProvider>{story()}</DictionaryProvider>],
+    args: {
+        section: '/about',
+        links: [
+            { id: 'home', label: 'Home', path: '/home' },
+            { id: 'about', label: 'About', path: '/about' },
+            { id: 'blog', label: 'Blog', path: '/blog' },
+        ],
+    },
+} as ComponentMeta<typeof NavBar>;
+
+const Template: ComponentStory<typeof NavBar> = (args) => {
+    const [section, setSection] = useState(args.section);
+
+    return (
+        <NavBar
+            {...args}
+            section={section}
+            onClick={(path) => {
+                setSection(path);
+            }}
+        />
+    );
+};
+
+export const Primary = Template.bind({});
